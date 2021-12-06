e2ae93614e1eb0c360d2653a3ff55bad00d338f9
Adrienne Meltzer <you@example.com>
2021-12-06 17:28:00 -0500

    Homework update

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://media.istockphoto.com/photos/curious-kitten-looking-at-the-camera-picture-id1270023880?b=1&k=20&m=1270023880&s=170667a&w=0&h=7tAE4L2d7qJ4k1OMydwof1kObkG6TuNkDj2n2QC88ic=';
+  image3 = 'https://media.istockphoto.com/photos/kitten-looking-up-picture-id1281700863?b=1&k=20&m=1281700863&s=170667a&w=0&h=-ikJUy0bE3hxaVARLuiE3bo4ovced0XJUL3UtVVHXA0=';
 
   constructor() { }
 
